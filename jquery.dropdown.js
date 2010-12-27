87dd29f21e52c72d39529850897a7b907eec422d
Sukjoon Kim <sjoonk@gmail.com>
2010-12-28 02:56:53 +0900

    set zindex value

--- jquery.dropdown.js
+++ jquery.dropdown.js
@@ -3,13 +3,14 @@
 		var settings = $.extend({
 			align: 'left',
 			speed: 'normal',
-			toggledClass: 'closed'
+			toggledClass: 'closed',
+			zIndex: 10
 		}, options || {});
 
 		return this.each(function() {
 			var $trigger = $(this), $target = $trigger.next();
 			$target.addClass('toggle-drop').css({display: 'none', position: 'absolute', margin: 0})
-			.css(settings.align, 0);
+			.css(settings.align, 0).css('z-index', settings.zIndex);
 			$trigger
 			.wrap('<div class="dropdown-w" style="position: relative; display: inline;">')
 			.after($target)
